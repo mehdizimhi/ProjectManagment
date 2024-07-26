778e2b6295c0863071c7641d273cd9f81214895c
mehdizimhi <mehdizimhi001@gmail.com>
2024-07-26 12:51:42 -0700

    Jwt config

---
+++ SystemManagmentProjects/src/main/java/com/mehdi/SysManagment/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.mehdi.SysManagment.config;
+
+public class JwtConstant {
+
+	public static final String SECRETE_KEY = "zgergre rg ers gt sr gth ershg ert h rrh zre t";
+	
+	public static final String JWT_HEADER = "Authorization";
+}
